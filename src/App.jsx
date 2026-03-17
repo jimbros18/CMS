7c77cbc9b0d251b9a1332d4bd2e026d173b2d643
jimbros18 <kingmelky09@gmail.com>
2026-03-17 16:38:51 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import React from 'react';
+import './styles/app.css'
+import Sidebar from './Sidebar.jsx'
+import Content from './Content.jsx'
+
+function App() {
+ const [activeKey, setActiveKey] = React.useState("clients");
+
+  const handleItemClick = (key, func) => {
+    setActiveKey(key);
+    func();
+  };
+
+  return (
+    <>
+      <Sidebar 
+        activeKey={activeKey}
+        onItemClick={handleItemClick}
+      />
+      <main className="main-content">
+         <Content activeKey={activeKey} />
+      </main>
+    </>
+  )
+}
+
+export default App
